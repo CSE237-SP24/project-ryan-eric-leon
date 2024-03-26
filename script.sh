e2ed6da19ceb54e8ae9b2cce1736f4fa0b84e5eb
Eric Wei <eric.wei@wustl.edu>
2024-03-25 23:14:40 -0500

    script to compile and run program

--- script.sh
+++ script.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+cd src
+
 #javac command to compile
+echo "compiling java files..."
+javac bankapp/*.java
+echo "compiled successfully"
 
 #java to run the code
+java bankapp.Menu
+
+cd ..
